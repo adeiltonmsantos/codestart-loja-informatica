efec904f72aadd64948dacf0d800818ea6c91cb3
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2025-09-02 12:21:20 -0300

    Defining CPU and Monitor sections and sponsors slider

--- assets/script.js
+++ assets/script.js
@@ -5,5 +5,80 @@ document.addEventListener("DOMContentLoaded", function(){
     mobileMenuIcon.addEventListener('click', function(){
         menu.classList.toggle('mobile-menu-open');
     });
+})
 
+// Products Filter
+document.addEventListener("DOMContentLoaded", function(){
+    const sections = document.querySelectorAll(".products-code-start");
+
+    sections.forEach((section) => {
+        const menu = section.querySelector(".prodcut-filter-brands ul");
+        const menuItens = section.querySelectorAll(".prodcut-filter-brands ul li");
+        const productCards = section.querySelectorAll(".card-new-product");
+
+        const state = {
+            activeBrand: "todos",
+            activeType: "todos"
+        }
+
+        function updateCards(){
+            productCards.forEach(card =>{
+                const brand = card.getAttribute("data-brand");
+                const type = card.getAttribute("data-products-type");
+
+                if((state.activeBrand === "todos" || state.activeBrand === brand) && (state.activeType === "todos" || state.activeType === type)){
+                    card.style.display = "block";
+                }else{
+                    card.style.display = "none";
+                }
+            })
+        }
+
+        menuItens.forEach((item) => {
+            item.addEventListener("click", () => {
+                menuItens.forEach((menuItem) => {
+                    menuItem.classList.remove("product-brand-active");
+                })
+
+                item.classList.add("product-brand-active");
+
+                state.activeBrand = item.getAttribute("data-brand");
+                state.activeType = item.getAttribute("data-products-type");
+
+                updateCards();
+            })
+        })
+
+        updateCards();
+    
+    })
+})
+
+// Sponsors slider
+window.addEventListener("DOMContentLoaded", () => {
+    const slider = document.querySelector(".slider-sponsors");
+    const images = slider.querySelectorAll("img");
+    
+    images.forEach(image => {
+        const clone = image.cloneNode(true);
+        slider.appendChild(clone);
+    });
+
+    const totalWidth = images.length * (images[0].offsetWidth + 20);
+
+    slider.style.width = `${totalWidth}px`;
+    
+    let currentPosition = 0;
+
+    const moveSlider = () => {
+        currentPosition -= 1;
+        if(currentPosition <= -totalWidth/2){
+            currentPosition = 0;
+        }
+
+        slider.style.transform = `translateX(${currentPosition}px)`;
+        requestAnimationFrame(moveSlider);
+    };
+
+    requestAnimationFrame(moveSlider);
 })
