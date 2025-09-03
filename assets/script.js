d6b996a066cfe562244d9cec27504083e6639339
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2025-09-03 12:17:59 -0300

    Begin of sponsors slider refactoring

--- assets/script.js
+++ assets/script.js
@@ -55,30 +55,30 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 // Sponsors slider
-window.addEventListener("DOMContentLoaded", () => {
-    const slider = document.querySelector(".slider-sponsors");
-    const images = slider.querySelectorAll("img");
+// window.addEventListener("DOMContentLoaded", () => {
+//     const slider = document.querySelector(".slider-sponsors");
+//     const images = slider.querySelectorAll("img");
     
-    images.forEach(image => {
-        const clone = image.cloneNode(true);
-        slider.appendChild(clone);
-    });
+//     images.forEach(image => {
+//         const clone = image.cloneNode(true);
+//         slider.appendChild(clone);
+//     });
 
-    const totalWidth = images.length * (images[0].offsetWidth + 20);
+//     const totalWidth = images.length * (images[0].offsetWidth + 20);
 
-    slider.style.width = `${totalWidth}px`;
+//     slider.style.width = `${totalWidth}px`;
     
-    let currentPosition = 0;
+//     let currentPosition = 0;
 
-    const moveSlider = () => {
-        currentPosition -= 1;
-        if(currentPosition <= -totalWidth/2){
-            currentPosition = 0;
-        }
+//     const moveSlider = () => {
+//         currentPosition -= 1;
+//         if(currentPosition <= -totalWidth/2){
+//             currentPosition = 0;
+//         }
 
-        slider.style.transform = `translateX(${currentPosition}px)`;
-        requestAnimationFrame(moveSlider);
-    };
+//         slider.style.transform = `translateX(${currentPosition}px)`;
+//         requestAnimationFrame(moveSlider);
+//     };
 
-    requestAnimationFrame(moveSlider);
-})
+//     requestAnimationFrame(moveSlider);
+// })
